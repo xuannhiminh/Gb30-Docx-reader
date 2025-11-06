6b4099c4fd7a57cb083c4a9b601af005c3a9a294
Huy <huyhuy6620000@gmail.com>
2025-11-06 16:31:38 +0700

    Request full screen intent permission

--- base-module/src/main/java/com/ezteam/baseproject/utils/TemporaryStorage.kt
+++ base-module/src/main/java/com/ezteam/baseproject/utils/TemporaryStorage.kt
@@ -10,6 +10,7 @@ object TemporaryStorage {
      @JvmStatic var isObtainConsent: Boolean = false
      @JvmStatic @Volatile var isSavingFileNotNoti = false
      @JvmStatic var isShowedDefaultReaderRequestDialogInThisSession: Boolean = false
+     @JvmStatic var isShowedFullScreenRequestDialogInThisSession: Boolean = false
      @JvmStatic var isShowSatisfiedDialogInThisSession: Boolean = false
      @JvmStatic var isShowedAddToHoneDialog: Boolean = false
      @JvmStatic var timeEnterPdfDetail = 0;
@@ -27,6 +28,7 @@ object TemporaryStorage {
      @JvmStatic
      fun reset() {
         isShowedDefaultReaderRequestDialogInThisSession = false
+         isShowedFullScreenRequestDialogInThisSession = false
         isShowSatisfiedDialogInThisSession = false
         isShowedAddToHoneDialog = false
         timeEnterPdfDetail = 0
